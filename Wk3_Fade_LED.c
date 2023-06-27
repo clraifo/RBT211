fb5e4982eb628efccc5e8db9c07268a4fca0bba7
clraifo <41687507+clraifo@users.noreply.github.com>
2023-06-27 19:01:40 -0400

    Update Wk3_Fade_LED.c
    Final version for submission

--- Wk3_Fade_LED.c
+++ Wk3_Fade_LED.c
@@ -1,60 +1,75 @@
-/*
-This one WORKS! KEEP IT!
-*/
-
+//Include necessary libraries
+#include <avr/io.h>         // Standard AVR header, allows I/O
+#include <avr/interrupt.h>  // Allows for use of interrupts
 #define F_CPU 16000000UL	// Set the CPU clock speed to 16MHz
+#include <util/delay.h>     // Allows for use of _delay_ms() function
 
-#include <avr/io.h>			// Standard AVR header, allows I/O
-#include <util/delay.h>		// Allows for use of _delay_ms() function
-#include <avr/interrupt.h>	// Allows for use of interrupts
+// Define PWM values for fading LED
+#define HALF_PWM 127        // 50% of max brightness
+#define MAX_PWM 255		    // 100% of max brightness  
+#define MIN_PWM 0		    // 0% of max brightness
 
-#define MAX_PWM 255		// 100% of max brightness
-#define HALF_PWM 127	// 50% of max brightness	
-#define MIN_PWM 0		// 0% of max brightness
-#define QUARTER_PWM 64  // 25% of max brightness
+// Declare volatile variables to be used in the interrupt service routines (ISRs)
+volatile uint8_t fade = 0;         // Fade level of LED
+volatile uint8_t direction = 0;    // Direction of fade (0=up, 1=down)
+volatile uint8_t blink_state = 0;  // State of blink (0=off, 1=on)
 
-int main(void)	// Main function
+// Function to initialize Timer0
+void timer0_init()
 {
+    DDRD |= (1 << PD6) | (1 << PD5);                                        // Configure PD6 and PD5 as output
+    TCCR0A = (1 << COM0A1) | (1 << COM0B1) | (1 << WGM01) | (1 << WGM00);   // Set Timer0 to Phase Correct PWM Mode
+    TCCR0B = (1 << CS00);                                                   // Set the clock without any prescaler
+}
 
-	DDRD |= (1 << PORTD6);	//Set PD6 as output
-	DDRD |= (1 << PORTD5);	//Set PD5 as output
+// Function to initialize Timer1
+void timer1_init()
+{
+    TCCR1A = 0;                                         // Set Timer1 to Normal Mode
+    TCCR1B = (1 << WGM12) | (1 << CS12) | (1 << CS10);  // Set Timer1 to CTC Mode and pre-scaling of 1024
+    TIMSK1 = (1 << OCIE1A);                             // Enable interrupt when Timer1 matches OCR1A
+    OCR1A = 15624;                                      // Set output compare register to generate a 1s delay
+    sei();                                              // Enable global interrupts
+}
 
-	DDRB |= (1 << PORTB5);		//Set PB5 as output
-	PORTB &= ~(1 << PORTB5);	//Set PB5 to low
+// Main function
+int main(void)
+{
+    timer0_init(); // Initialize Timer0
+    timer1_init(); // Initialize Timer1
 
-	// Set Timer0 for Fast PWM mode and normal port operation, clear OC0A/OC0B on compare match
-	TCCR0A = (1 << COM0A1) | (1 << COM0B1) | (1 << WGM01) | (1 << WGM00);	//Set Timer0 for Fast PWM mode and normal port operation, clear OC0A/OC0B on compare match
+    while (1)       // Infinite loop
+    {
+        if (direction == 0) // If direction is up
+        {
+            if (fade < HALF_PWM)    // And fade level is less than 50%
+                fade++;             // Increase fade level
+            else
+                direction = 1;      // Else, change direction to down
+        }
+        else // If direction is down
+        {
+            if (fade > 0)           // And fade level is more than 0
+                fade--;             // Decrease fade level
+            else
+                direction = 0;      // Else, change direction to up
+        }
+        OCR0A = fade;               // Assign fade level to OCR0A (PD6)
+        _delay_ms(10);              // Delay for 10 ms
+    }
+}
 
-	// Set prescaler to 64 and start the timer
-	TCCR0B = (1 << CS01) | (1 << CS00);
-
-	while (1)	
-	{
-		for (uint8_t i = MIN_PWM; i < HALF_PWM; i++)	// Fade in PD6 to half brightness
-		{
-			OCR0A = i;	// Set the value for OC0A (PD6) PWM duty cycle
-			_delay_ms(2);	// Delay for 2 milliseconds
-		}
-
-		for (uint8_t i = HALF_PWM; i > MIN_PWM; i--)	// Fade out PD6
-		{
-			OCR0A = i;  	// Set the value for OC0A (PD6) PWM duty cycle
-			_delay_ms(2);	// Delay for 2 milliseconds
-		}
-
-		for (uint8_t i = MIN_PWM; i < MAX_PWM; i++)		// Fade in PD5 to full brightness
-		{
-			OCR0B = i;	// Set the value for OC0B (PD5) PWM duty cycle
-			_delay_ms(2);	// Delay for 2 milliseconds
-		}
-
-
-		for (uint8_t i = MAX_PWM; i > MIN_PWM; i--) 		// Fade out PD5
-		{
-			OCR0B = i;	// Set the value for OC0B (PD5) PWM duty cycle
-			_delay_ms(2);	// Delay for 2 milliseconds
-		}
-	}
-
-	return 0;	
+// ISR for Timer1 compare match
+ISR(TIMER1_COMPA_vect)
+{
+    if (blink_state == 0)   // If LED is off
+    {
+        OCR0B = HALF_PWM;   // Turn it on at 50% brightness
+        blink_state = 1;    // Set blink state to on
+    }
+    else // If LED is on
+    {
+        OCR0B = MIN_PWM;    // Turn it off
+        blink_state = 0;    // Set blink state to off
+    }
 }
